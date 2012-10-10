a13238a1d3970dda259b002841d0366f68479693
Hery <aeonzamaek@gmail.com>
2012-10-10 17:34:31 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,76 @@
+// server.js
+
+var status = 'Not connected';
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://nodejitsu:5b1e05df485363de011b314b9b0c9e71@alex.mongohq.com:10041/nodejitsudb520454785224', function (req,res) { 
+    console.log('Connected to db.');
+    status = 'Connected';
+});
+
+var express = require('express');
+var app = express();
+
+app.use(express.bodyParser());
+app.use(express.cookieParser());    
+app.use(express.session({secret:'my secret'}));
+
+app.use(function(req,res,next) {
+    if (req.session.loggedIn) {
+        res.locals.authenticated = true;
+        User.findById(req.session.loggedIn, function(err,doc) {
+            if (err) return next(err);
+            res.locals.me = doc;
+            next();
+        });
+    } else {
+        res.locals.authenticated = false;
+        next();
+    }
+});
+
+app.set('view engine', 'jade');
+app.set('view options', {layout: false});
+
+app.get('/', function (req,res) {
+    res.render('index', {status: status});
+});
+
+app.get('/login', function (req,res) {
+    res.render('login', {signupEmail:''});
+});
+
+app.get('/login/:signupEmail', function (req,res) {
+    res.render('login', {signupEmail: req.params.signupEmail});
+});
+
+app.post('/login', function (req,res) {
+    User.findOne({ email: req.body.user.email, password: req.body.user.password }, function (err,doc) {
+        if (err) return next(err);
+        if (!doc) return res.send('<p>User not found. Go back and try again</p>');
+        req.session.loggedIn = doc._id.toString();  
+        res.redirect('/');
+    });
+});
+
+app.get('/signup', function(req,res) {
+    res.render('signup');
+});
+
+app.post('/signup', function(req,res,next) {
+    var user = new User(req.body.user).save(function(err, user) {    
+        if (err) return next(err);
+        res.redirect('/login/' + user.email);
+    });
+});
+
+app.listen('3000');
+
+var Schema = mongoose.Schema
+
+var User = mongoose.model('User', new Schema({
+    first: String,
+    last: String,
+    email: {type:String, unique:true},
+    password: {type:String,index:true}
+}));
