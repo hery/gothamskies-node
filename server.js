d15716d120dec2c0b7400e35ee2a56d264abf9b3
pandaman <hratsim00@ccny.cuny.edu>
2012-12-24 03:35:28 -0500

    commit

--- server.js
+++ server.js
@@ -1,42 +1,61 @@
 // server.js
 
+var mongoose = require('mongoose');
 var status = 'Not connected';
 
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://pandaman:ilovebamboo@linus.mongohq.com:10077/app10212447', function (req,res) { 
+// Remote db for production environment
+// mongoose.connect('mongodb://pandaman:ilovebamboo@linus.mongohq.com:10077/app10212447', function (req,res) { 
+//     console.log('Connected to db.');
+//     status = 'Connected';
+// });
+
+// local db for developing environment
+mongoose.connect('mongodb://localhost/local', function (req,res) { 
     console.log('Connected to db.');
     status = 'Connected';
 });
 
 var express = require('express');
+// Use RedisStore to avoid default MemoryStore warning in production environment
+var RedisStore = require('connect-redis')(express);
 var app = express();
 
-var MemJS = require("memjs").Client;
-memjs = MemJS.create()
+// Was going to use MemJS as a MemoryStore, but no.
+// var MemJS = require("memjs").Client;
+// memjs = MemJS.create()
 
 app.use(express.bodyParser());
 app.use(express.cookieParser());    
-// app.use(express.session({secret:'beautiful panda'}));
 
-// app.use(function(req,res,next) {
-//      if (req.session.loggedIn) {
-//         res.locals.authenticated = true;
-//         User.findById(req.session.loggedIn, function(err,doc) {
-//             if (err) return next(err);
-//             res.locals.me = doc;
-//             next();
-//         });
-//     } else {
-//         res.locals.authenticated = false;
-//         next();
-//     }
-// });
+// app.use(express.session({secret:'beautiful panda'}));
+app.use(express.session({store: new RedisStore, secret:'beautiful panda'}));
+
+// Authentication middleware // Needs some review
+app.use(function(req,res,next) {
+     if (req.session.loggedIn) {
+        res.locals.authenticated = true;
+        User.findById(req.session.loggedIn, function(err,doc) {
+            if (err) return next(err);
+            res.locals.me = doc;
+            next();
+        });
+    } else {
+        res.locals.authenticated = false;
+        next();
+    }
+});
 
 app.set('view engine', 'jade');
 app.set('view options', {layout: false});
 
 app.get('/', function (req,res) {
-    res.render('index', {status: status});
+    if (req.session.loggedIn) {
+    	username = res.locals.me.first;
+	usertype = res.locals.me.type;
+    	res.render('index', {status: status, accountType: usertype, username: username});
+    } else {
+    	res.render('index');
+    }
 });
 
 app.get('/login', function (req,res) {
@@ -51,7 +70,7 @@ app.post('/login', function (req,res) {
     User.findOne({ email: req.body.user.email, password: req.body.user.password }, function (err,doc) {
         if (err) return next(err);
         if (!doc) return res.send('<p>User not found. Go back and try again</p>');
-//        req.session.loggedIn = doc._id.toString();  
+        req.session.loggedIn = doc._id.toString();  
         res.redirect('/');
     });
 });
@@ -68,7 +87,7 @@ app.post('/signup', function(req,res,next) {
 });
 
 app.get('/logout', function(req,res) {
-//     req.session.loggedIn = null;
+    req.session.loggedIn = null;
     res.redirect('/');
 });
 
@@ -83,5 +102,14 @@ var User = mongoose.model('User', new Schema({
     first: String,
     last: String,
     email: {type:String, unique:true},
-    password: {type:String,index:true}
+    password: {type:String,index:true},
+    type: String
+}));
+
+var Root = mongoose.model('Root', new Schema({
+    first: String,
+    last: String,
+    email: {type:String, unique:true},
+    password: {type:String,index:true},
+    type: String
 }));
